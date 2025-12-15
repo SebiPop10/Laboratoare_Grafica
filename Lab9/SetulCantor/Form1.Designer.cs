4dcc271fdfc1f3862b8025d780bf05735b0f7d07
SebiPop10 <92030844+SebiPop10@users.noreply.github.com>
2025-12-15 21:55:31 +0200

    Add files via upload

---
+++ Lab9/SetulCantor/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace SetulCantor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBarIteratii = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarIteratii)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBarIteratii
+            // 
+            this.trackBarIteratii.Location = new System.Drawing.Point(594, 373);
+            this.trackBarIteratii.Name = "trackBarIteratii";
+            this.trackBarIteratii.Size = new System.Drawing.Size(104, 56);
+            this.trackBarIteratii.TabIndex = 0;
+            this.trackBarIteratii.Scroll += new System.EventHandler(this.trackBarIteratii_Scroll);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 401);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 54);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1347, 467);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.trackBarIteratii);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarIteratii)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBarIteratii;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Lab9/SetulCantor/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SetulCantor
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Setul Cantor";
+            this.Width = 800;
+            this.Height = 600;
+            this.DoubleBuffered = true;
+           
+            this.ResumeLayout(false);
+        }
+
+        public List<(double, double)> GenereazaCantor(int iteratii)
+        {
+            var segmente = new List<(double, double)>
+            {
+                (0.0, 1.0)
+            };
+
+            for(int i=0;i<iteratii;i++)
+            {
+                var segmenteNoi = new List<(double, double)>();
+                foreach (var segment in segmente)
+                {
+                    double stanga = segment.Item1;
+                    double dreapta = segment.Item2;
+                    double oTreime = (dreapta - stanga)/3.0;
+
+                    segmenteNoi.Add((stanga, stanga + oTreime));
+                    segmenteNoi.Add((dreapta - oTreime, dreapta));
+                }
+                segmente = segmenteNoi;
+            }
+            return segmente;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //varianta fara slider pentru nr de iteratii
+            //int nrIteratii = 5;
+
+            //varianta cu slider pentru nr de iteratii
+            int nrIteratii = trackBarIteratii.Value;
+            int inaltimeSegment = 20;
+            int spatiuVertical = 10;
+
+            int latimeFormular= this.ClientSize.Width;
+
+            for(int nivel=0; nivel<=nrIteratii; nivel++)
+            {
+                var segmente = GenereazaCantor(nivel);
+                int y = nivel * (inaltimeSegment + spatiuVertical) + spatiuVertical;
+                foreach(var segment in segmente)
+                {
+                    double stanga = segment.Item1;
+                    double dreapta = segment.Item2;
+                    int x1 = (int)(stanga * latimeFormular);
+                    int x2 = (int)(dreapta * latimeFormular);
+                    //e.Graphics.FillRectangle(Brushes.Black, x1, y, x2 - x1, inaltimeSegment);
+                    e.Graphics.DrawLine(Pens.Black, x1, y, x2, y);
+                }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            trackBarIteratii.Value=trackBarIteratii.Value+1;
+            //this.Paint+= new PaintEventHandler(Form1_Paint);
+            this.Invalidate();
+        }
+
+        private void trackBarIteratii_Scroll(object sender, EventArgs e)
+        {
+            
+                
+        }
+    }
+}
